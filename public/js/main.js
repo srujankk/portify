1d19956d1eb6cbaca4032ca50b5f1f49148c65d8
SRUJAN KUMAR KEDALA <srujankk@gmail.com>
2015-08-14 01:22:35 -0400

    updated authentication

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,19 @@
 /* global $ */
-$(function(){
-	$('#login').click(function(){
-		$(".login-container").hide();
-		$(".app-container").show();
-		window.location.hash = "dashboard";
-	})
+$(function () {
+    $('#login').click(function () {
+        //$(".login-container").hide();
+        //$(".app-container").show();
+        //window.location.hash = "dashboard";
+        var val = {
+            "username": $("#uname_txt").val(),
+            "password": $("#pwd_txt").val()
+        }
+        $.post('http://localhost:3000/login', val).done(function (data) {
+            console.log("Login Successful: ");
+            console.log(data);
+        }).fail(function (err) {
+            console.log("login failed: ");
+            console.log(err);
+        })
+    })
 })
